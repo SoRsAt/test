dcdb3653ebd0d7679a19209c56438f9664e99c57
Gambol00 <92166459+Gambol00@users.noreply.github.com>
2021-10-10 01:42:41 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -17312,15 +17312,10 @@ local Teext =[[
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{
 {text = '✯اوامر المطورين✯', callback_data="/help5"},{text = '✯اوامر التسليه✯', callback_data="/help7"},
-},
-{
 {text = '✯اوامر الاعضاء✯', callback_data="/help6"},
-},
-{
 {text = '✯اوامر التعطيل و التفعيل✯', callback_data="/help2"},{text = '✯اوامر القفل و الفتح✯', callback_data="/help1"},
-{
+}
 Return Https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(Teext)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard))
 end
 
