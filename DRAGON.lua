b2211c00ae928a7958e3556fe7e683445e15b181
Gambol00 <92166459+Gambol00@users.noreply.github.com>
2021-10-10 09:47:55 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -18117,7 +18117,6 @@ keyboard.inline_keyboard = {
 {{text = 'ǫᴏʀsᴀɴ',url="t.me/S_79_Q"}},
 {{text = 'ʙᴀʀ', callback_data="/barqors"}},{{text = 'ʙᴏᴛs', callback_data="/botqors"}},
 {
-local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/S_79_Q&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 
