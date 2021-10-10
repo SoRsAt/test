a8f50dba6745d6e4f334117216507f9012784ea7
Gambol00 <92166459+Gambol00@users.noreply.github.com>
2021-10-10 13:32:31 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -14139,18 +14139,13 @@ local Groups = bot_data:scard(bot_id..'Chek:Groups')
 local Users = bot_data:scard(bot_id..'User_Bot')  
 keyboard = {} 
 keyboard.inline_keyboard = {
-{
 {text = '✯ عدد الجروبات  ➠ {'..Groups..'}'
-},
-{
 {text = '✯  عدد المشتركين  ➠ {'..Users..'}'
-},
 }
 local function getpro(extra, result, success) 
 if result.photos_[0] then 
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&caption=' .. URL.escape(Namebot).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
-
 if text == 'الجروبات' then
 if Sudo(msg) then 
 local Groups = bot_data:scard(ban_id..'Chek:Groups')  
