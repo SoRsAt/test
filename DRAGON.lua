197d2b5abd053c71ee09077ddd13439390adf699
Gambol00 <92166459+Gambol00@users.noreply.github.com>
2021-10-10 02:12:11 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -17641,7 +17641,7 @@ keyboard.inline_keyboard = {
 {text = 'sᴏᴜʀᴄᴇ ᴍᴀᴛʀɪx', url="t.me/G8_01"},
 },
 {
-return https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(Teext)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
+Return https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(Teext)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
 if Text == '/change-photo' then
 local Teext =[[
